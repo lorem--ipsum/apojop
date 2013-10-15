5c842317dae90fd3369ab466f7632d38ceedc081
Sébastien Fragnaud <sebastien.fragnaud@expandium.com>
2013-10-15 13:20:29 +0200

    Demo almost done

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,18 @@ angular.module('npp', ['ng-pojo-print'])
 })
 
 .controller('DemoCtrl', function($scope) {
-  $scope.examples = [
-    {foo: 'bar'}
-  ];
+  $scope.max_columns = 80;
+  $scope.max_depth = 2;
+  
+  $scope.example = {
+    axes: {
+      x: {type: "date", tooltipFormatter: function (d) {return moment(d).fromNow();}, key: "x"},
+      y: {type: "linear"}
+    },
+    series: [
+      {y: "val_0", label: "A time series", color: "#9467bd"}
+    ],
+    lineMode: "linear",
+    tooltipMode: "default"
+  };
 });
