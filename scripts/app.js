c7533735619eba522c7c2aec742fd466d8e4f691
Sébastien Fragnaud <sebastien.fragnaud@expandium.com>
2013-10-15 11:43:09 +0200

    WIP

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,11 @@
 angular.module('npp', ['ng-pojo-print'])
 
+.config(function config($routeProvider) {
+  $routeProvider
+    .when('/', {controller: 'DemoCtrl', templateUrl: 'views/main.html'})
+    .otherwise({redirectTo: '/'});
+})
+
 .controller('DemoCtrl', function($scope) {
   $scope.examples = [
     {foo: 'bar'}
