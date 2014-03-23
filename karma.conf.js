8f0dc2346b6560842470ab738690de8afdd718ee
Sébastien <sebastien.fragnaud@metamarkets.com>
2014-03-23 13:55:35 +0100

    Upgraded to latest libs

--- karma.conf.js
+++ karma.conf.js
@@ -1,64 +1,74 @@
-// base path, that will be used to resolve files and exclude
-basePath = '';
+// Karma configuration
+// Generated on Sun Mar 23 2014 12:23:58 GMT+0100 (CET)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'bower_components/angular/angular.js',
-  'bower_components/angular-mocks/angular-mocks.js',
-  'apojop.min.js',
-  '/tmp/apojop.spec.js'
-];
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
 
-preprocessors = {
-  'apojop.min.js': 'coverage'
+
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/angular/angular.min.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'apojop.min.js',
+      '/tmp/apojop.spec.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'apojop.min.js': 'coverage'
+    },
+
+    coverageReporter: {
+      type : 'text-summary',
+      dir : 'coverage/'
+    },
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['dots', 'coverage'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
 };
-
-// list of files to exclude
-exclude = [
-];
-
-
-coverageReporter = {
-  type : 'text-summary',
-  dir : 'coverage/'
-};
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots', 'coverage'];
-
-
-// web server port
-port = 9876;
-
-
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-// browsers = ['PhantomJS'];
-browsers = ['PhantomJS']//, 'Firefox']
